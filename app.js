6f2afd5cfd01ed26a55a44dd0639624d711554ce
Manar Abdsalam <69817727+manarabdsalam@users.noreply.github.com>
2024-01-09 22:31:14 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,34 @@
+const formOpenBtn = document.querySelector("#form-open"),
+    home = document.querySelector(".home"),
+    formContainer = document.querySelector(".form_container"),
+    formCloseBtn = document.querySelector(".form_close"),
+    signupBtn = document.querySelector("#signup"),
+    loginBtn = document.querySelector("#login"),
+    pwShowHide = document.querySelector(".pw_hide");
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+pwShowHide.forEcach((icon) => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if (getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil_eye");
+        }else{
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil_eye-slash");
+        }
+    });
+});
+
+
+signupBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.add("active");
+});
+
+loginBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    formContainer.classList.remove("active");
+});
